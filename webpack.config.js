a03206731309577a271b13bed00b15635fb26ce6
maple3142 <kirito_@mail.edu.tw>
2017-09-15 19:45:34 +0800

    initial

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path')
+var webpack = require('webpack')
+var prod = (process.env.NODE_ENV === 'production')
+
+module.exports = {
+	entry: path.join(__dirname, 'src', 'index.js'),
+	output: {
+		filename: prod ? 'vuejs-storage.min.js' : 'vuejs-storage.js',
+		path: path.join(__dirname, 'dist'),
+		libraryTarget: 'umd',
+		library: 'vuejsStorage'
+	},
+	module: {
+		loaders: [
+			{
+				loader: 'babel-loader',
+				test: /\.js/,
+				exclude: /node_modules/
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.js']
+	},
+	devtool: 'source-map',
+	plugins: prod ? [
+		new webpack.optimize.UglifyJsPlugin({
+			compress: { warnings: false },
+			sourceMap: true
+		})
+	] : []
+}
