86d5f76b71ff898f339b3ee7a9ab242d751adc3c
maple3142 <kirby741852963@gmail.com>
2018-02-05 11:42:30 +0800

    config karma for travis-ci

--- karma.conf.js
+++ karma.conf.js
@@ -1,25 +1,21 @@
-module.exports = function (config) {
-	config.set({
+module.exports = function(config) {
+	const cfgs = {
 		basePath: '',
 		frameworks: ['mocha', 'chai', 'karma-typescript'],
-		files: [
-			'test/**/*.ts',
-			'src/**/*.ts'
-		],
-		exclude: [
-		],
+		files: ['test/**/*.ts', 'src/**/*.ts'],
+		exclude: [],
 		preprocessors: {
 			'**/*.ts': ['karma-typescript']
 		},
 		karmaTypescriptConfig: {
 			bundlerOptions: {
 				entrypoints: /test.*\.ts$/,
-				exclude: ["vue/types/vue", "vuex/types/index"]
+				exclude: ['vue/types/vue', 'vuex/types/index']
 			},
 			compilerOptions: {
 				module: 'commonjs'
 			},
-			tsconfig: './tsconfig.json',
+			tsconfig: './tsconfig.json'
 		},
 		reporters: ['progress', 'karma-typescript'],
 		port: 9876,
@@ -28,6 +24,16 @@ module.exports = function (config) {
 		autoWatch: true,
 		browsers: ['Chrome'],
 		singleRun: false,
-		concurrency: Infinity
-	})
+		concurrency: Infinity,
+		customLaunchers: {
+			Chrome_travis_ci: {
+				base: 'Chrome',
+				flags: ['--no-sandbox']
+			}
+		}
+	}
+	if (process.env.TRAVIS) {
+		cfgs.browsers = ['Chrome_travis_ci']
+	}
+	config.set(cfgs)
 }
