1ff28c155e2189fbee85423659a3d84a3062fee7
maple3142 <kirby741852963@gmail.com>
2018-07-06 20:16:43 +0800

    refactor install & vuexplugin to use driver

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
-import { Vue, VueConstructor, VuexPlugin } from './interfaces'
+import { Vue, VueConstructor, VuexPlugin, StorageDriver } from './interfaces'
 
 import { install } from './install'
 import { createVuexPlugin } from './vuexplugin'
+import * as drivers from './drivers'
 
 interface vjs {
 	(): VuexPlugin<Object>
-	install: (Vue: VueConstructor) => any
+	install: (Vue: VueConstructor) => void
+	drivers: { localStorage: StorageDriver; sessionStorage: StorageDriver }
 }
 const vuejsStorage = <vjs>function(option) {
 	return createVuexPlugin(option)
 }
 vuejsStorage.install = install
+vuejsStorage.drivers = drivers
 
 export default vuejsStorage
