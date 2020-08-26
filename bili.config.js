f287a75cad363e3f191034827908a6207bc7a121
maple3142 <kirby741852963@gmail.com>
2020-08-26 14:40:30 +0800

    Fix dist exports

--- bili.config.js
+++ bili.config.js
@@ -1,4 +1,10 @@
 module.exports = {
 	format: ['umd', 'umd-min', 'es', 'cjs'],
-	input: 'src/index.ts'
+	input: 'src/index.ts',
+	output: {
+		dir: 'dist',
+		moduleName: 'vuejsStorage',
+		fileName: 'vuejs-storage.[format][min][ext]',
+		format: ['umd', 'cjs', 'es', 'umd-min', 'cjs-min', 'es-min'],
+	},
 }
