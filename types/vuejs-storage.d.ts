075f169dda327e4cdb55d8e84645dfbbc080a0f0
maple3142 <kirby741852963@gmail.com>
2020-09-16 00:32:44 +0800

    Fix Typescript typing

--- types/vuejs-storage.d.ts
+++ types/vuejs-storage.d.ts
@@ -1,14 +1,9 @@
-/* main.js */
 import Vue from 'vue/types/index'
-import Vuex from 'vuex/types/index'
-import { Plugin as VuexPlugin } from 'vuex/types/index'
-import { StorageDriver, Option } from '../src/interfaces'
-import { vjs } from '../src/index'
+import { Option } from '../src/interfaces'
+import vjs from '../src/index'
 
-/* entry.js */
 export = vjs
 
-/* override vue option */
 declare module 'vue/types/options' {
 	interface ComponentOptions<V extends Vue> {
 		storage?: Option | Option[]
